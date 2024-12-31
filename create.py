6b8c19b6925dc4f5a0f6b7117881f96196e0f49a
Harikrishna S <harikrishnas.x@gmail.com>
2024-12-31 12:40:07 +0530

    Modified create.py

--- create.py
+++ create.py
@@ -19,6 +19,16 @@ try:
                 f.write("#include <iostream>\n\nint main() {\n\n\treturn 0;\n}")
         else:
             print(f"> File {path} already exists")
+    
+    for j in range(2):
+        path = f"./Day{i}/input{j+1}.txt"
+
+        if not os.path.exists(path) or clear == "Yes":
+            with open(path, "w") as f:
+                print(f"> Created file {path}")
+                f.write("#include <iostream>\n\nint main() {\n\n\treturn 0;\n}")
+        else:
+            print(f"> File {path} already exists")
 
     print("Created files")
 except Exception as e:
